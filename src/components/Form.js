e4adb7eb876742a992ce6a57a43cb5bf3bb9bd36
Md Aatif Tegginmani <mdaatif84@gmail.com>
2023-05-09 22:36:42 +0530

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,15 @@
 import './Form.css'
-import React, { useRef } from 'react';
+import React, { useRef ,useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 
 
 const Form = () => {
   const form = useRef();
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [message, setMessage] = useState('');
+
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -14,23 +18,27 @@ const Form = () => {
       .then((result) => {
           console.log(result.text);
           console.log("message sent");
+          setUserName('');
+          setUserEmail('');
+          setMessage('');
       }, (error) => {
           console.log(error.text);
       });
+
   };
   return (
     <form className='form' ref={form} onSubmit={sendEmail}>
         <label htmlFor="">Your Name</label>
-        <input type="text" name='user_name' />
+        <input type="text" name='user_name' value={userName} onChange={(e) => setUserName(e.target.value)} />
 
         <label htmlFor="">Email</label>
-        <input type="email" name='user_email' />
+        <input type="email" name='user_email' value={userEmail} onChange={(e) => setUserEmail(e.target.value)} />
 
         {/*<label htmlFor="">Subject</label>
         <input type="text" name='user_subject' />
   */}
         <label htmlFor="">Message</label>
-        <textarea name="message" id=""   rows="6" placeholder='message here'></textarea >
+        <textarea name="message" id=""   rows="6" placeholder='message here' value={message} onChange={(e) => setMessage(e.target.value)}></textarea >
 
         <button  className='btn'  value="Send" >Submit</button>
     </form>
